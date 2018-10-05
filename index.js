abb1f05ba8eab464871c959d1445f0795f9601c1
shijiebei2014 <1611491782@qq.com>
2018-10-05 12:59:38 +0800

    fix set

--- index.js
+++ index.js
@@ -11,22 +11,23 @@ const kvo = (context, path, value) => {
   const segments = path.split('.')
   const isGetter = util.isUndefined(value)
   return segments.reduce((memo, key, index) => {
-    // console.log('key:%s, memo:%j', key, memo)
     var next = nextFn(memo, key)
-    // console.log('isGetter:%b, next:%j', isGetter, next)
     if (segments.length - 1 === index) { // 出口
       if (isGetter) { // getter
         memo = next
       } else { // setter
-        // console.log('key:%s, memo:%j', key, memo, value)
-        if (util.isNull(memo)) {
+        if (util.isNull(memo) || util.isUndefined(memo)) {
           memo = {}
         }
         memo[key] = value
-        // console.log(memo)
       }
     } else {
-      memo = next
+      if (util.isNull(next) || util.isUndefined(next)) {
+          memo[key] = {}
+      } else {
+          memo[key] = next
+      }
+      return memo[key]
     }
     return memo
   }, context)
